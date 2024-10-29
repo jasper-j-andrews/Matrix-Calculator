abec60bc802bda82bf9abf6758e16e720de055fc
Jasper <jasperandrews@me.com>
2024-10-29 10:26:04 -0700

    Test Cases

--- matrix_multiplication.py
+++ matrix_multiplication.py
@@ -13,8 +13,12 @@ def calculate_product(A, B):
                 sum += A[i][x]*B[x][j]
             c_row.append(sum)
         C.append(c_row)
+    print(C)
     return C
 
 calculate_product(X, Y)
 calculate_product(Z, X)
 calculate_product(X, Z)
+calculate_product([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[-1, 0, 0],[0, 2, 0], [0, 0, 5]])
+calculate_product([[2, 1, 2], [3, 0, -1], [8, 1, 7]], [[2], [-9], [5]])
+calculate_product([[2, 5, 6], [1, 0, -2], [-1, -2, 4]], [[1, 2, 1], [0, -3, -1], [1, 0, 0]])
